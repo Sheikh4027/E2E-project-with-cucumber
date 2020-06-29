b28a0fe052e6cbc6874b5dc613e35e3fb6f3c7b7
sheikh4027@gmail.com <sheikh4027@gmail.com>
2020-06-29 09:22:34 -0400

    Signed-off-by: sheikh4027@gmail.com <sheikh4027@gmail.com>

---
+++ src/main/java/resources/base.java
@@ -0,0 +1,63 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class base {
+
+	  public WebDriver driver;
+	public Properties prop;
+public WebDriver initializeDriver() throws IOException
+{
+	
+ prop= new Properties();
+FileInputStream fis=new FileInputStream("C:\\Users\\ahmad\\E2EProject\\src\\main\\java\\resources\\data.properties");
+
+prop.load(fis);
+String browserName=prop.getProperty("browser");
+System.out.println(browserName);
+
+if(browserName.equals("chrome"))
+{
+	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\ahmad\\Downloads\\Documents\\chromedriver.exe");
+	driver= new ChromeDriver();
+		//execute in chrome driver
+	
+}
+else if (browserName.equals("firefox"))
+{
+	 driver= new FirefoxDriver();
+	//firefox code
+}
+else if (browserName.equals("IE"))
+{
+//	IE code
+}
+
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+return driver;
+
+
+}
+
+public void getScreenshot(String result) throws IOException
+{
+	File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	FileHandler.copy(src, new File("C://test//"+result+"screenshot.png"));
+	
+}
+
+
+}
